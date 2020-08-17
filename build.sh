d005f84daec4f4def94ae82179657e86a6292565
starudream <justwangsheng@qq.com>
2020-08-17 18:12:07 +0800

    feat: enable auto push

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ cd "./${NAME}"
 
 echo -e "\033[34m[ Start '${NAME}' ]\033[0m"
 
-docker build --force-rm --compress -q -t registry.cn-shanghai.aliyuncs.com/vsi-open/"${NAME}":latest .
+docker build --force-rm --compress -t registry.cn-shanghai.aliyuncs.com/vsi-open/"${NAME}":latest .
 
 echo -e "\033[32m[ Login Aliyun Docker ]\033[0m"
 docker login -u "${ALIYUN_DOCKER_USERNAME}" -p "${ALIYUN_DOCKER_PASSWORD}" registry.cn-shanghai.aliyuncs.com
-#echo -e "\033[32m[ Publish Aliyun Docker ]\033[0m"
-#docker push registry.cn-shanghai.aliyuncs.com/vsi-open/"${NAME}":latest
+echo -e "\033[32m[ Publish Aliyun Docker ]\033[0m"
+docker push registry.cn-shanghai.aliyuncs.com/vsi-open/"${NAME}":latest
 
 echo -e "\033[34m[ End '${NAME}' ]\033[0m"
 
