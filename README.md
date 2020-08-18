2e3c144e07ff9b296b06a85c7990dd59b26e3770
starudream <justwangsheng@qq.com>
2020-08-18 14:59:14 +0800

    feat: change docker registry

--- README.md
+++ README.md
@@ -15,18 +15,3 @@
      ↓      ↓
     node  flutter
 ```
-
-```bash
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/ci-mysql
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/ci-tidb
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/alpine
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/alpine-glibc
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/alpine-glibc-grpc
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/ci-bot
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/protogen
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/go-dev
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/node
-docker pull registry-vpc.cn-shanghai.aliyuncs.com/vsi-open/flutter
-
-docker image prune
-```
--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ cd "./${NAME}"
 
 echo -e "\033[34m[ Start '${NAME}' ]\033[0m"
 
-docker build --force-rm --compress -t registry.cn-shanghai.aliyuncs.com/vsi-open/"${NAME}:${VERSION}" .
+docker build --force-rm --compress -t izhaohucom/"${NAME}:${VERSION}" .
 
-echo -e "\033[32m[ Login Aliyun Docker ]\033[0m"
-docker login -u "${ALIYUN_DOCKER_USERNAME}" -p "${ALIYUN_DOCKER_PASSWORD}" registry.cn-shanghai.aliyuncs.com
-echo -e "\033[32m[ Publish Aliyun Docker ]\033[0m"
-docker push registry.cn-shanghai.aliyuncs.com/vsi-open/"${NAME}:${VERSION}"
+echo -e "\033[32m[ Login Docker ]\033[0m"
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+echo -e "\033[32m[ Publish Docker ]\033[0m"
+docker push izhaohucom/"${NAME}:${VERSION}"
 
 echo -e "\033[34m[ End '${NAME}' ]\033[0m"
 
