f74076fa381af972de24751194d24f55cfb553e4
starudream <justwangsheng@qq.com>
2020-08-31 11:41:21 +0800

    perf: perfect ci

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo -e "\033[32m[ Login ]\033[0m"
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
 VERSION=v1.18.8
-echo -e "\033[32m[ '${VERSION}' ]\033[0m"
+echo -e "\033[32m[ ${VERSION} ]\033[0m"
 
 wget -qO ./kubeadm https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kubeadm
 chmod +x ./kubeadm
@@ -16,11 +16,11 @@ chmod +x ./kubeadm
 repositories=$(./kubeadm config images list --kubernetes-version=$VERSION)
 
 while IFS='/' read -r user repository; do
-  echo -e "\033[34m[ '${repository}' ]\033[0m"
+  echo -e "\033[34m[ ${repository} ]\033[0m"
   echo -e "\033[32m[ Pull ]\033[0m"
   docker pull "${user}"/"${repository}"
   docker tag "${user}"/"${repository}" izhaohucom/"${repository}"
-  echo -e "\033[32m[ Publish ]\033[0m"
+  echo -e "\033[32m[ Push ]\033[0m"
   docker push izhaohucom/"${repository}"
 done <<<"${repositories}"
 
