8e1ec673dcd2c5d411f66191604c23472197a799
starudream <justwangsheng@qq.com>
2020-11-16 15:32:52 +0800

    feat: update 1.18.12

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ echo -e "\033[34m[ Start ]\033[0m"
 echo -e "\033[32m[ Login ]\033[0m"
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
-VERSION=v1.18.11
+VERSION=v1.18.12
 echo -e "\033[32m[ ${VERSION} ]\033[0m"
 
 wget -qO ./kubeadm https://storage.googleapis.com/kubernetes-release/release/$VERSION/bin/linux/amd64/kubeadm
